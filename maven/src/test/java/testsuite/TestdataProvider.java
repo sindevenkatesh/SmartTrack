b020afee5b5383e67a0e10b078c2d68c39fbbe58
venkateshshinde <venkateshshinde@LSLT-IT10452>
2018-09-27 18:20:50 +0530

    DataProvider Class Added

---
+++ maven/src/test/java/testsuite/TestdataProvider.java
@@ -0,0 +1,49 @@
+package testsuite;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import config.Constants;
+
+public class TestdataProvider extends Constants{
+
+	@Test(dataProvider = "sampTest")
+	public static void login(String loginName, String password) {		
+		System.out.println("User Name :" + loginName);
+		System.out.println("Password :" + password);
+		System.out.println("*************************");
+	}
+
+	public static Object[][] XlReading() throws IOException {
+		Object abc[][] = null;
+		try {
+			FileInputStream fis = new FileInputStream(ExcelPath);
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			XSSFSheet st = wb.getSheetAt(4);
+			int Rowcount = st.getLastRowNum();
+			int COlumncount = st.getRow(0).getLastCellNum();
+			abc = new Object[Rowcount][COlumncount];
+			
+			for (int i = 1; i <= Rowcount; i++) {
+				for (int j = 0; j < COlumncount; j++) {
+					abc[i - 1][j] = st.getRow(i).getCell(j).getStringCellValue();					
+					//System.out.println("value is :"+value);
+					//abc[i-1][j]= value.toString();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return abc;
+	}
+	@DataProvider(name = "sampTest", parallel = false)
+	public static Object[][] testParameterData() throws Exception {// Data provider annotation usage starts here
+		Object data[][] = XlReading();
+		return data;
+	}
+}
