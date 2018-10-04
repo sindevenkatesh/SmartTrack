bf6431de298c02d1f6af98e39dabc995bba18670
venkateshshinde <venkateshshinde@LSLT-IT10452>
2018-10-04 17:40:00 +0530

    ScreenShot Class added

---
+++ maven/src/test/java/utility/screenShot.java
@@ -0,0 +1,18 @@
+package utility;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class screenShot {
+
+	public void takeScreenShot(WebDriver driver, String path, String fileName) throws IOException {
+
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyf
+
+	}
+}
