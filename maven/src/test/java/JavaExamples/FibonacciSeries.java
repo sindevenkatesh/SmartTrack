df32608c7192b1d8e8559c26650db13d6bafab33
venkateshshinde <venkateshshinde@LSLT-IT10452>
2018-11-28 20:31:26 +0530

    fibonacci

--- maven/src/test/java/JavaExamples/FibonacciSeries.java
+++ maven/src/test/java/JavaExamples/FibonacciSeries.java
@@ -1,36 +1,22 @@
 package JavaExamples;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-
 public class FibonacciSeries {
-	
-	@Test
-	public void fibonacci() {
-		
-		int n=10,j=0,k=1;
-		
-		for(int i=1;i<=n;i++) {
-			
-			int sum=j+k;
-			j=k;
-			k=sum;		
-			System.out.println("Fibonacci : "+k);						
-		}
-	}
-	
-	@Test
-	public void fibo() {
-		int i=1,n=10,j=0,k=1;
-		while(i<=n) {
-			int add=j+k;
-			j=k;
-			k=add;
-			System.out.println("fibo :"+k);
-			i++;
-		}
-	}
-	
 
+	public static void main(String[] args) {
+
+		int n = 10, a = 0, b = 1;
+
+		int sum;
+
+		for (int i = 0; i <= n; i++) {
+
+			sum = a + b;
+			
+			a = b;
+			b = sum;
+			
+			System.out.println(sum);
+
+		}
+	}
 }
